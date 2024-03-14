89ca1da73a290a59db9f0cdaebd71872394e009f
Amir Mohana <62573376+amirmohana@users.noreply.github.com>
2024-03-14 19:24:23 -0300

    Add files via upload

---
+++ Desafio Javascript/main.js
@@ -0,0 +1,88 @@
+// 1 - Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+//Respota:
+//alert("Hello Word")
+
+// 2 - Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+//Respota:
+// let x = 10;
+// let y = 20;
+
+// let soma = x + y 
+
+// alert("O resultado da soma é: " + soma)
+
+// 3 - Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+//Respota:
+// var x = "42"; // Pode ser qualquer valor, por exemplo, "42" ou 42
+
+// if (typeof x === "number" || !isNaN(Number(x))) {
+//     console.log("É um número.");
+// } else {
+//     console.log("Não é um número.");          
+// }
+
+// 4 - Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+//Respota:
+// let x = "Exercicio 4"
+
+// if(typeof x === "string" ){
+//   alert("É uma string")
+// }
+// else{
+//   alert("Não é uma string")
+// }
+
+// 5 - Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+//Respota:
+// let x =  true
+
+// if(typeof x === "boolean" ){
+//   alert("É booleano")
+// }
+// else{
+//   alert("Não é um booleano")
+// }
+//  6 - Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+//Respota:
+// let x = 10;
+// let y = 40 
+
+// let subtração = x - y;
+
+// alert("O resultado da subtração é : " + subtração)
+
+//7 - Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+// let x = 10;
+// let y = 40 
+
+// let multiplicação = x * y;
+
+// alert("O resultado da multiplicação é : " + multiplicação)
+
+// 8 - Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+// let x = 10;
+// let y = 40 
+
+// let divisão = x / y;
+
+// alert("O resultado da divisão é : " + divisão)
+
+// 9 - Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+// let x = 10;
+
+// if(x % 2 === 0){
+//  alert("Numero par")
+// }
+// else{
+//   alert("Numero impar")
+// }
+
+// 10 -  Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+// let x = 11;
+
+// if(x % 2 === 0){
+//  alert("Numero impar")
+// }
+// else{
+//   alert("Numero par")
+// }
